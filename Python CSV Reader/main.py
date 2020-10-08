a9b2eb0f2533fefb23e4e33be596272372a3cc4d
Johanthan Kunz <johnathan.kunz@aggiemail.usu.edu>
2020-10-08 13:53:37 -0600

    got the python program done, and an output csv file, need to manually add host city data

--- Python CSV Reader/main.py
+++ Python CSV Reader/main.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 gamesFile = open("athlete_events.csv")
 
@@ -17,18 +18,53 @@ for line in gamesFile:
     team = lineList[6]
     noc = lineList[7]
     games = lineList[8]
+    year = lineList[9]
     city = lineList[11]
     medal = lineList[14]
 
-    if not(games in gamesDict):
-        print(games)
+    if not(year in gamesDict):
         # the games dict should include
-        #0          1      2
-        #Country    city   CountryDict, holding medal count
-        gamesDict[games] = [city, dict()]
-    curGameArray = gamesDict[games]
+        #0          1       2
+        #GameName   Year    CountryDict, holding medal count
+        gamesDict[year] = [games, year, dict()]
 
+    curGamesDict = gamesDict.get(year)
+    curGamesDictCompleteNOCMedals = curGamesDict[2]
+    if not(noc in curGamesDictCompleteNOCMedals):
+        curGamesDictCompleteNOCMedals[noc] = 0
 
+    if(medal == "Gold"):
+        curGamesDictCompleteNOCMedals[noc] += 1
+    curGamesDict[2] = curGamesDictCompleteNOCMedals
+    gamesDict[year] = curGamesDict
 gamesFile.close()
 
-print(gamesDict)
+dictKeysSorted = sorted(gamesDict.keys())
+
+print(len(dictKeysSorted))
+with open('output2.csv', mode='w') as outputFile:
+    csvOutWriter = csv.writer(outputFile, delimiter=',')
+    csvOutWriter.writerow(["Game", "Year",  "NumberOfCountries", "AttendingCountriesNOC", "MaxMedalCountry","MaxMedalCount", "RunnerUpMedalCountry", "RunnerUpMedalCount", "GameCountryNOC"])
+    for key in dictKeysSorted:
+        curElement = gamesDict[key]
+        countryMedalDict = curElement[2]
+
+        maxVal = 0
+        maxCountry = ""
+        secondaryMaxVal = 0
+        secondMaxCountry = ""
+
+        for key in countryMedalDict.keys():
+            curVal = countryMedalDict[key]
+            if(curVal > maxVal):
+                maxVal = curVal
+                maxCountry = key
+            elif(curVal > secondaryMaxVal):
+                secondaryMaxVal = curVal
+                secondMaxCountry = key
+
+
+        # 0          1       2
+        # GameName   Year    CountryDict, holding medal count
+        keylistOfCountries =  str(sorted(countryMedalDict.keys())).replace("[", "").replace("]","").replace("'","")
+        csvOutWriter.writerow([curElement[0], curElement[1], countryMedalDict.keys().__len__(), keylistOfCountries , maxCountry, maxVal, secondMaxCountry, secondaryMaxVal])
